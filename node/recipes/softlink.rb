cdde2cd1ca1939cd0ce7ccadfdeb5bec629d164f
Khaled Attia <khaledattia@gmail.com>
2014-12-13 15:22:36 +0200

    [Improved] node soft link

--- node/recipes/softlink.rb
+++ node/recipes/softlink.rb
@@ -2,5 +2,7 @@ node[:deploy].each do |application, deploy|
   execute "node-softlink" do
     command "ln -s \"$(which nodejs)\" /usr/bin/node"
     user "root"
+    
+    not_if do ::File.exists?('/usr/bin/node') end
   end
 end
